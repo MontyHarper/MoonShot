4a5fa8881dbb1ff40ff8dcbef7bc4d4241411c30
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2023-09-01 22:05:05 -0500

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Monty Harper on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
